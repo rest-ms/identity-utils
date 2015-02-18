b9ef6554a803b6a1e4ca0620b39700121d041701
Alessandro Siletto <alessandro@siletto.it>
2015-02-18 20:24:28 +0100

    handled realm per service

--- src/main/java/it/siletto/ms/identity/service/IdentityDAO.java
+++ src/main/java/it/siletto/ms/identity/service/IdentityDAO.java
@@ -4,5 +4,5 @@ import it.siletto.ms.identity.model.Identity;
 
 public interface IdentityDAO {
 
-	public Identity getUser(String username, String password);
+	public Identity getUser(String username, String password, String realm);
 }
--- src/main/java/it/siletto/ms/identity/service/impl/IdentityDAOMockImpl.java
+++ src/main/java/it/siletto/ms/identity/service/impl/IdentityDAOMockImpl.java
@@ -10,9 +10,9 @@ import com.google.common.collect.Sets;
 public class IdentityDAOMockImpl implements IdentityDAO {
 
 	@Override
-	public Identity getUser(String username, String password) {
+	public Identity getUser(String username, String password, String realm) {
 		Identity ret = null;
-		if("pippo".equals(username) && "pluto".equals(password)){
+		if("pippo".equals(username) && "pluto".equals(password) && "test".equals(realm)){
 			ret = new Identity();
 			ret.setUsername(username);
 			Set<String> roles = Sets.newHashSet();
